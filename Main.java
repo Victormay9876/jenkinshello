48fdf8da474fde64db24f56ab7e97a51ad492a8e
Victormay9876 <victor.may9876@gmail.com>
2025-03-07 17:36:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        system.out.println("i am chitti,the AI powered ROBOT ");
+        System.out.println("i am chitti,the AI powered ROBOT ");
     }
 
 
